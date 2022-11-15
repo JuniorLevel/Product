5840e01deba4e7830141647e75f0b18ddb80794f
junior <work.junior@mail.ru>
2022-11-15 19:48:05 +0300

    Добавлена тёмная тема

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,10 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+const HTML = document.documentElement;
+const btnSwitchTheme = document.querySelector('.action-header__btnSwitchTheme');
+
+btnSwitchTheme.addEventListener("click", function() {
+	HTML.classList.toggle('darkTheme');	
+})
